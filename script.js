48d21afd7ce642efbb9e060b72020d0e19bea9ac
eunanath22 <128197391+eunanath22@users.noreply.github.com>
2024-06-21 22:29:06 -0300

    s

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-const caixaPrincipal = document.querySelector(".")
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = docunt.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = domunt.querySelector("texto-resultado");
+
+const perguntas =[
+   enunciado: "Pergunta 1",
+alternativas:[
+    "Alternativa 1",
+     "A;ternativa 2"
+    ]
+     
+    
+    "Pergunta 2"
+    ];
+    const perguntas = [
+        {
+            enunciado: "Pergunta 1",
+            alternativas: [
+                "Alternativa 1",
+                "Alternativa 2"
+            ]
+        },
+        {
+            enunciado: "Pergunta 2",
+            alternativas: [
+                "Alternativa 1",
+                "Alternativa 2"
+            ]
+        }
+    ]; 
+    
